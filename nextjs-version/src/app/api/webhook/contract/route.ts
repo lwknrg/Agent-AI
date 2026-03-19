73093ef89d29a8a72a12043b80deadce095b328e
lwknrg <lwknrg@gmail.com>
2026-03-19 13:34:52 +0700

    ok

--- nextjs-version/src/app/api/webhook/contract/route.ts
+++ nextjs-version/src/app/api/webhook/contract/route.ts
@@ -7,7 +7,7 @@ const prisma = new PrismaClient()
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string)
 
 cloudinary.config({
-  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 })
